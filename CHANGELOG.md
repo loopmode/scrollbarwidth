41fc40a5c06fecd34eaac183287e61164155af04
Jovica Aleksic <jovica.aleksic@xailabs.de>
2018-08-09 12:30:42 +0200

    1.1.0: optional `target` argument

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+### 1.1.0
+
+*   Added optional `target` argument for checking a specific element for scrollbar width
--- scrollbarwidth.js
+++ scrollbarwidth.js
@@ -1,18 +1,26 @@
 (function() {
-    'use strict';
+    "use strict";
 
-    function scrollbarwidth() {
-        return window.innerWidth - document.documentElement.clientWidth;
+    /**
+     * Measures the width of the scrollbar of a given element, or of the global window itself when called without arguments.
+     *
+     * @param {Element} [target] - Container element to check. Defaults to `window.documentElement`.
+     * @return {Number} The width of the scrollbar in pixels. If there is no scrollbar, 0 is returned.
+     */
+    function scrollbarwidth(target) {
+        if (target) {
+            return target.offsetWidth - target.clientWIdth;
+        }
+        return window.innerWidth - window.documentElement.clientWidth;
     }
 
-    if (typeof define === 'function' && define.amd) {
+    if (typeof define === "function" && define.amd) {
         define([], function() {
             return scrollbarwidth;
         });
-    } else if (typeof exports !== 'undefined') {
+    } else if (typeof exports !== "undefined") {
         module.exports = scrollbarwidth;
     } else {
         this.scrollbarwidth = scrollbarwidth;
     }
-
-}).call(this);
+}.call(this));
